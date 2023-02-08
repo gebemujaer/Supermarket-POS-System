6ca57558d158df0b0bd2e316eaa5f1eea2b399d6
gebemujaer <108812009+gebemujaer@users.noreply.github.com>
2023-02-08 12:30:41 +0700

    Create mermaid.js

---
+++ mermaid.js
@@ -0,0 +1,3 @@
+<link rel="stylesheet" href="https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.min.css" />
+<script src="https://unpkg.com/mermaid@8.0.0-rc.8/dist/mermaid.min.js"></script>
+<script>mermaid.initialize({startOnLoad:true});</script>
